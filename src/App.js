fc118e5ad167df775063528c360c081f9c79478a
yoho1017 <hide1017@yahoo.com.tw>
2021-06-08 01:11:09 +0800

    feat: modeify Btn01Example and gsap3xScript.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './css/App.css'
-import { Bnt01Example, Bnt02Example, CoinExample,UiExample } from './lib/gsap3xScript'
+import { Btn01Example, Btn02Example, CoinExample,UiExample} from './lib/gsap3xScript'
 // import { CoinAnimation } from './lib/gsap2xScript'
 
 function App() {
 
   return (
     <div className="App">
-      <Bnt01Example />
-      <Bnt02Example />
+      <Btn01Example />
+      <Btn02Example />
       <CoinExample title="You win" /> 
       <UiExample/>
        {/* <CoinAnimation /> */}
--- src/lib/gsap3xScript.js
+++ src/lib/gsap3xScript.js
@@ -4,108 +4,115 @@ import button03On from '../imgs/button03_on.png'
 import coin from '../imgs/coin.png'
 import football from '../imgs/football.png'
 import basketball from '../imgs/basketball.png'
+import baseball from '../imgs/baseball.png'
 
-export function Bnt01Example() {
-  const headerRef = useRef(null)
-  const btnRef = useRef(null)
-  const [background, setBackground] = useState('rgb(255, 255, 255)')
-  const [fontcolor, setColor] = useState('rgb(77, 50, 151)')
-  const toggleBackground = () => {
-    const fontColor2 = fontcolor !== 'rgb(77, 50, 151)' ? 'rgb(77, 50, 151)' : 'rgb(155, 255, 255)'
-    const color = background !== 'rgb(255, 255, 255)' ? 'rgb(255, 255, 255)' : 'rgb(0, 0, 0)'
-    setBackground(color)
-    setColor(fontColor2)
-  }
 
-  useEffect(() => {
-    gsap.to(headerRef.current, {
-      duration: 1,
-      backgroundColor: background,
-      ease: 'none'
+export function SvgExample() {
+    const svgref=useRef()
+    useEffect(()=>{
+      gsap.to(svgref.current,{
+        repeat:-1,
+        attr:{   
+          width:"100"
+      }})
     })
-  }, [background])
-
-  useEffect(() => {
-    gsap.to(btnRef.current, {
-      duration: 0.1,
-      color: fontcolor,
-      ease: 'none'
-    })
-  }, [fontcolor])
-
-  useEffect(() => {
-    gsap.fromTo(
-      '.bnt1',
-      {
-        y: -50,
-        opacity: 0
-      },
-      {
-        y: 100,
-        opacity: 1,
-        stagger: 0.1,
-        boxShadow: '0px 0px 10px rgb(91, 224, 224),0px 0px 20px rgb(91, 224, 224)'
-      },
-      '-=1'
-    )
-  }, [])
-
-  return (
-    <div className="header-block" ref={headerRef}>
-      <button className="bnt1" ref={btnRef} onClick={toggleBackground}>
-        change background
-      </button>
+  return(
+    <div className='header-block black'>  
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 841.9 595.3">
+        <g fill="#61DAFB">
+            <rect x="-25" y="-25" width="200" height="100" ref={svgref}/>
+        </g>
+      </svg>
     </div>
   )
 }
 
-export function Bnt02Example() {
-  const bnt02Ref = useRef(null)
-  useEffect(() => {
-    const timeline = gsap.timeline({
-      repeat: -1,
-      defaults: {
-        duration: 0.5,
-        ease: 'none'
-      }
-    })
-    const timeline2 = gsap.timeline({ duration: 1, ease: 'none' })
-    timeline2.fromTo('.bnt2-block', { x: -100, opacity: 0 }, { duration: 0.5, x: 0, opacity: 1, stagger: 0.2 })
-    timeline
-      .fromTo('span:nth-child(1)', { x: -150, opacity: 1 }, { x: 50, opacity: 0.5 }, 0)
-      .fromTo('span:nth-child(2)', { y: -50, opacity: 1 }, { y: 50, opacity: 0.5 }, 0.3)
-      .fromTo('span:nth-child(3)', { x: 55, opacity: 1 }, { x: -100, opacity: 0.5 }, 0.6)
-      .fromTo('span:nth-child(4)', { y: 50, opacity: 1 }, { y: -50, opacity: 0.5 }, 1)
-  }, [])
-
-  const bnt2Click = e => {
-    const newId = e.target.id
-    const el = document.querySelectorAll('.bnt2-block')[newId]
-    gsap.from(el, { top: '20px' })
+export function Btn01Example() {
+  const headerRef = useRef(null)
+  const btnRef = useRef(null)
+  const [background, setBackground] = useState('rgb(0, 0, 0)')
+  const [fontcolor, setColor] = useState('rgb(109, 136, 255)')
+  const [bntColor, setbntColor] = useState('black')
+  const [bntBorder,setbntBorder]=useState('3px solid rgb(54, 225, 255)')
+  const toggleBackground = () => {
+    const backgroundChange = background !== 'rgb(0, 0, 0)' ? 'rgb(0, 0, 0)' : 'rgb(255, 255, 255)' 
+    const fontColorChange = fontcolor !== 'rgb(109, 136, 255)' ? 'rgb(109, 136, 255)' : '#f8a942'
+    const bntColorChange= bntColor !== 'black'?'black':'rgb(253, 255, 109)'
+    const bntBorderChange=bntBorder !== '3px solid rgb(54, 225, 255)'?'3px solid rgb(54, 225, 255)':'3px solid rgb(255, 128, 54)'
+    setBackground(backgroundChange)
+    setColor(fontColorChange)
+    setbntColor(bntColorChange)
+    setbntBorder(bntBorderChange)
+    gsap.from(btnRef.current,{y:20,rotate:180})
   }
 
-  const Bnt2 = [...Array(3).keys()]
+  useEffect(() => {
+    gsap.to(headerRef.current, {duration: 1,backgroundColor: background,ease: 'none'})
+  }, [background])
+
+  useEffect(() => {
+    gsap.to(btnRef.current,{ duration: 0.2,color: fontcolor, backgroundColor:bntColor,border:bntBorder,ease:'none'})
+  }, [fontcolor,bntColor,bntBorder])
+
+  return (
+    <div className="header-block" ref={headerRef}>
+      <div className="btn1" ref={btnRef} onClick={toggleBackground}>
+       <h2>Changebackground</h2>
+      </div>
+    </div>
+  )
+}
+
+export function Btn02Example() {
+
+  const Btn02Component=(props)=>{
+    const btn02Ref = useRef(null)
+    useEffect(() => {
+      const timeline = gsap.timeline({
+        repeat: -1,
+        defaults: {
+          duration: 0.5,
+          ease: 'none'
+        }
+      })
+      const timeline2 = gsap.timeline({ duration: 1, ease: 'none' })
+      timeline2.fromTo( '.btn2-block', { x: -100, opacity: 0 }, { duration: 0.5, x: 0, opacity: 1, stagger: 0.2 })
+      timeline
+        .fromTo('span:nth-child(1)', { x: -150, opacity: 1 }, { x: 50, opacity: 0.5 }, 0)
+        .fromTo('span:nth-child(2)', { y: -50, opacity: 1 }, { y: 50, opacity: 0.5 }, 0.3)
+        .fromTo('span:nth-child(3)', { x: 55, opacity: 1 }, { x: -100, opacity: 0.5 }, 0.6)
+        .fromTo('span:nth-child(4)', { y: 50, opacity: 1 }, { y: -50, opacity: 0.5 }, 1)
+    }, [])
+
+    const btn2Click = () => {
+      gsap.from( btn02Ref.current, { top: '20px' })
+    }
+
+    return (
+      <div className='btn2-block' ref={btn02Ref}  onClick={btn2Click}>
+        <div className="btn2List-block">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <p>{props.text}</p>
+        </div>
+     </div>
+    )
+  }
+  
+  const Btn2 = [{text:'今日'},{text:'滾球'},{text:'冠軍'}]
   return (
     <div className="header-block black">
-      {Bnt2.map(value => {
-        return (
-          <div className={`bnt2-block ${value}`} key={value} ref={bnt02Ref} onClick={bnt2Click}>
-            <div className="bnt2List-block">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <p id={value}>button02_{value}</p>
-            </div>
-          </div>
-        )
+      {Btn2.map((value,key)=>{
+        return <Btn02Component text={value.text} key={key}/>
       })}
     </div>
   )
 }
 
 export function CoinExample(props) {
-  const bnt03Ref = useRef(null)
+  const btn03Ref = useRef(null)
   useEffect(() => {
     const random = gsap.utils.random(-500, 500, 100, true)
     const random02 = gsap.utils.random(0, 360, 30, true)
@@ -121,7 +128,7 @@ export function CoinExample(props) {
       ,0 0 10px rgb(101, 245, 245)`
     timeline
       .fromTo(
-        ['.bnt3-block', 'h1.title'],
+        ['.btn3-block', 'h1.title'],
         { scale: 0, opacity: 0 },
         { duration: 1, scale: 1, opacity: 1, ease: 'elastic.out(1, 0.3)' }
       )
@@ -134,9 +141,9 @@ export function CoinExample(props) {
   }, [])
   const boxArray = [...Array(30).keys()]
   return (
-    <div className="header-block black" ref={bnt03Ref}>
-      <div className="bnt3-block">
-        <img className="bnt3img" src={button03On} alt="button03-on" />
+    <div className="header-block black" ref={btn03Ref}>
+      <div className="btn3-block">
+        <img className="btn3img" src={button03On} alt="button03-on" />
         <h1 className="title">{props.title}</h1>
         {boxArray.map(value => {
           return (
@@ -150,15 +157,15 @@ export function CoinExample(props) {
   )
 }
 
-
 export function UiExample(){ 
-  const Ballicon=(props)=>{
+  const BalliconComponent=(props)=>{
     const uiRef=useRef(null) 
     const uiTween = useRef()
     const ballRef=useRef(null) 
     const ballTween = useRef()
     const ballTextRef=useRef(null) 
     const ballTextTween = useRef()
+
     useEffect(()=>{
       uiTween.current=gsap.to(uiRef.current,{
        duration:0.5,
@@ -201,7 +208,7 @@ export function UiExample(){
     
     return(
       <div className='ui-block' onClick={ ballcionClick} onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler} ref={uiRef}>
-        <h2 ref={ballTextRef}>{props.text}</h2>
+        <h3 ref={ballTextRef}>{props.text}</h3>
         <div className='ball' ref={ballRef}>
           <img src={props.src}  alt="ball"/>         
         </div>        
@@ -209,11 +216,11 @@ export function UiExample(){
     )   
   }
   
-  const ballIconImgs=[{src:football,text:'football'},{src:basketball,text:'basketball'}]
+  const ballIconImgs=[{src:football,text:'football'},{src:basketball,text:'basketball'},{src:baseball,text:'baseball'}]
   return(
     <div className='header-block black'>
       {ballIconImgs.map((value,key)=>{
-       return <Ballicon src={value.src} key={key} text={value.text} />
+       return <BalliconComponent src={value.src} key={key} text={value.text} />
       })}
     </div>
   )
